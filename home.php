851437d1503af0ddd96ed74ca0967faaa831ebbf
BilguunBarsbold <bekprivate0207@gmail.com>
2021-05-04 10:58:16 +0800

    'update'

--- home.php
+++ home.php
@@ -1,51 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-        <title>Home</title>
-    </head>
-    <body>
-        <div class='container'>
-            <div class='row'>
-                <div class='col-12'>
-                    <h1>Welcome</h1>
-                    <?php include 'navbar.php'; ?>
+<?php include 'includes/header.php';?>
+    <h1>Welcome</h1>
+    <?php include 'includes/navbar.php'; ?>
 
-                    <form action='/register.php' method='POST'>
-                        <div class="name">
-                            <label for="exampleInputName">Enter your name</label>
-                            <input type="text" name='name' required class="form-control" id="exampleInputUsername">
-                        </div>
-                        <div class="username">
-                            <label for="exampleInputUsername">Username</label>
-                            <input type="text" name='username' required class="form-control" id="exampleInputUsername">
-                        </div>
-                        <div class="email">
-                            <label for="exampleInputEmail1">Email address</label>
-                            <input type="email" name="email" required class="form-control" id="exampleInputEmail1">
-                        </div>
-                        <div class="password">
-                            <label for="exampleInputPassword1">Password</label>
-                            <input type="password" name='password' required class="form-control" id="exampleInputPassword1">
-                        </div>
-                        <div class="conf_password">
-                            <label for="exampleInputPassword1">Confirm password</label>
-                            <input type="password" name='conf_password' required class="form-control" id="exampleInputPassword1">
-                        </div>
-                        <div class="button form-check">
-                            <input type="checkbox" required class="form-check-input" id="exampleCheck1">
-                            <label class="form-check-label" for="exampleCheck1">Terms and Policy</label>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
-                </div>
-            </div>
+    <form action='/register.php' method='POST'>
+        <div class="name">
+            <label for="exampleInputName">Enter your name</label>
+            <input type="text" name='name' required class="form-control" id="exampleInputUsername">
         </div>
-
-
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-    </body>
-</html>
+        <div class="username">
+            <label for="exampleInputUsername">Username</label>
+            <input type="text" name='username' required class="form-control" id="exampleInputUsername">
+        </div>
+        <div class="email">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" name="email" required class="form-control" id="exampleInputEmail1">
+        </div>
+        <div class="password">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" name='password' required class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="conf_password">
+            <label for="exampleInputPassword1">Confirm password</label>
+            <input type="password" name='conf_password' required class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="button form-check">
+            <input type="checkbox" required class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Terms and Policy</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>             
+<?php include 'includes/footer.php'; ?>
